2965e1560d9a9af74e78a2d4fee16f05ab2353b0
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-10-30 22:58:39 +0200

    refactoe app's navigation

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../Features/splash/presentation/views/splash_view.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/HomeView';
+  static final router = GoRouter(routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: '/HomeView',
+      builder: (context, state) => const HomeView(),
+    ),
+  ]);
+}
