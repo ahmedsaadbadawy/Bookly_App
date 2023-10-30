9efb4f0a3ddc6cae43980c28b9851fdd17a94acd
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-10-30 22:43:50 +0200

    create book detail view

---
+++ lib/Features/home/presentation/views/book_detail_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/book_detail_view_body.dart';
+
+class BookDetailView extends StatelessWidget {
+  const BookDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailViewBody(),
+    );
+  }
+}
