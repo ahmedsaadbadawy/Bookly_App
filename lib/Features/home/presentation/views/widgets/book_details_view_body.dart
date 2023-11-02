42a763530bd61b022bc70b9b5cd71700c5f87522
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-11-03 00:44:25 +0200

    create similar books list view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
+import 'similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -38,8 +39,20 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
           const SizedBox(height: 37),
           const BooksAction(),
+          const Expanded(child: SizedBox(height: 50)),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const SimilarBooksListview(),
+          const SizedBox(height: 40),
         ],
       ),
     );
   }
 }
+
