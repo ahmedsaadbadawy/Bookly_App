4af400afa5daf4cf04e9257b8c09bd85e9fc3b21
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-11-03 01:43:09 +0200

    search view done

--- lib/Features/search/presentaion/views/widgets/search_view_body.dart
+++ lib/Features/search/presentaion/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,10 +12,37 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(height: 16),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 16),
+          Expanded(child: SearchResultListView()),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BestSellerItem(),
+        );
+      },
     );
   }
 }
