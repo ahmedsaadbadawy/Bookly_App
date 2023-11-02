5e525d259aa0b9b11391550da48da228d9fd0470
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-11-03 00:28:32 +0200

    create books action

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -20,9 +21,7 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: const SafeArea(
-              child: CustomBookImage(),
-            ),
+            child: const CustomBookImage(),
           ),
           const SizedBox(height: 43),
           const Text('The Jingle Book', style: Styles.textStyle30),
@@ -37,6 +36,8 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           const SizedBox(height: 18),
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+          const SizedBox(height: 37),
+          const BooksAction(),
         ],
       ),
     );
