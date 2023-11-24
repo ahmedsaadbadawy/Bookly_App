912829b888b715c5de4ac3c49f384b2a57d08399
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-11-24 20:18:19 +0200

    implement remote fetch newest books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/core/utils/api_services.dart';
-
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -14,16 +13,15 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks() async {
     var data = await apiServise.get(endpoint: '');
     List<BookEntity> books = getBooksList(data);
-
     return books;
   }
 
   
-
   @override
-  Future<List<BookEntity>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchNewestBooks() async{
+    var data = await apiServise.get(endpoint: '');
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
 
 
